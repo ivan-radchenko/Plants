ee980fea983a9b495ad6622a165c0c057055ba1c
Ivan <sonysakura.rf@gmail.com>
2023-12-15 01:35:44 +0300

    php update

--- app/Livewire/CareToday.php
+++ app/Livewire/CareToday.php
@@ -2,11 +2,27 @@
 
 namespace App\Livewire;
 
+use App\Models\Plants;
 use App\Services\PlantsCare;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class CareToday extends Component
 {
+
+    public function wateringThis($plantID)
+    {
+        $plant=Plants::find($plantID);
+        if (\Auth::user()->id === $plant->userID){
+            /*$plant->update(['lastWatering' => Carbon::now()->toDateString()]);*/
+            request()->session()->flash('success','Растенька полита');
+        }
+    }
+
+    public function mount()
+    {
+
+    }
     public function render()
     {
         return view('livewire.care-today',[
--- resources/views/livewire/care-today.blade.php
+++ resources/views/livewire/care-today.blade.php
@@ -5,47 +5,47 @@
         @if($plants)
             <h3>Полив:</h3>
             <div class="cards-wrapper">
-                    @foreach($plants as $plant)
-                        <div wire:key="{{$plant->id}}"  class="plant-card">
-                            <img class="cart-image" src="{{Storage::disk('public')->url($plant->image)}}" alt="plant">
-                            <div>{{$plant->name}}</div>
-                            <button type="button">полить</button>
-                        </div>
-                    @endforeach
+                @foreach($plants as $plant)
+                    <div wire:key="{{$plant->id}}"  class="plant-card">
+                        <img class="cart-image" src="{{Storage::disk('public')->url($plant->image)}}" alt="plant">
+                        <div>{{$plant->name}}</div>
+                        <button wire:click="wateringThis({{$plant->id}})" type="button">полить</button>
+                    </div>
+                @endforeach
             </div>
-            <button type="button">полить все</button>
+            <button wire:click="" type="button">полить все</button>
         @else
             <div>just расслабься</div>
         @endif
         </div>
         <div>
         @if($changeLighting)
-                <h3 class="light-title">Освещение:</h3>
+            <h3 class="light-title">Освещение:</h3>
                 @foreach($changeLighting as $plantGroup)
                     <div>
-                    @if(\App\Services\SeasonNow::season() === \App\Enums\PlantStatus::SUMMER->value)
-                        <div wire:key="lightGroup-{{$plantGroup[0]->lightSummer}}">
-                            <h4 class="lightGroup">
-                                Увеличить освещение до {{$plantGroup[0]->lightSummer}}ч.
-                            </h4>
-                            <button>перевести группу на летнее освещение</button>
-                        </div>
-                    @else
-                        <div wire:key="lightGroup-{{$plantGroup[0]->lightWinter}}">
-                            <h4 class="lightGroup">
-                                Сократить освещение до {{$plantGroup[0]->lightWinter}}ч.
-                            </h4>
-                            <button>перевести группу на зимнее освещение</button>
-                        </div>
-                    @endif
+                        @if(\App\Services\SeasonNow::season() === \App\Enums\PlantStatus::SUMMER->value)
+                            <div wire:key="lightGroup-{{$plantGroup[0]->lightSummer}}">
+                                <h4 class="lightGroup">
+                                    Увеличить освещение до {{$plantGroup[0]->lightSummer}}ч.
+                                </h4>
+                                <button>перевести группу на летнее освещение</button>
+                            </div>
+                        @else
+                            <div wire:key="lightGroup-{{$plantGroup[0]->lightWinter}}">
+                                <h4 class="lightGroup">
+                                    Сократить освещение до {{$plantGroup[0]->lightWinter}}ч.
+                                </h4>
+                                <button>перевести группу на зимнее освещение</button>
+                            </div>
+                        @endif
                     </div>
                     <div class="cards-wrapper">
-                    @foreach($plantGroup as $plant)
-                        <div wire:key="light-{{$plant->id}}" class="plant-card">
-                            <img class="cart-image" src="{{Storage::disk('public')->url($plant->image)}}" alt="plant">
-                            <div>{{$plant->name}}</div>
-                        </div>
-                    @endforeach
+                        @foreach($plantGroup as $plant)
+                            <div wire:key="light-{{$plant->id}}" class="plant-card">
+                                <img class="cart-image" src="{{Storage::disk('public')->url($plant->image)}}" alt="plant">
+                                <div>{{$plant->name}}</div>
+                            </div>
+                        @endforeach
                     </div>
                 @endforeach
         @endif
