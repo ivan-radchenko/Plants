f77506961d9c77f6822b9ab7fd9adf81c7746335
Ivan <sonysakura.rf@gmail.com>
2023-12-16 23:48:22 +0300

    модальное окно на страницу поиска

--- resources/views/livewire/like-other.blade.php
+++ resources/views/livewire/like-other.blade.php
@@ -11,20 +11,35 @@
         <div class="stats-wrapper">
             @if($averageStats)
                 <h3>Усредненная статистика {{$searchInput}}:</h3>
-                <div class="modal-text">Время между поливами летом: {{$averageStats['waterSummer']}} дн.</div>
-                <div class="modal-text">Время между поливами зимой: {{$averageStats['waterWinter']}} дн.</div>
-                <div class="modal-text">Свет летом: {{$averageStats['lightSummer']}} час.</div>
-                <div class="modal-text">Свет зимой: {{$averageStats['lightWinter']}} час.</div>
-                <div class="modal-text">интенсивность освещения: {{$averageStats['light']}} </div>
-                <div class="modal-text">влажность: {{$averageStats['wet']}} </div>
+                <div class="stat-text">Время между поливами летом: {{$averageStats['waterSummer']}} дн.</div>
+                <div class="stat-text">Время между поливами зимой: {{$averageStats['waterWinter']}} дн.</div>
+                <div class="stat-text">Свет летом: {{$averageStats['lightSummer']}} час.</div>
+                <div class="stat-text">Свет зимой: {{$averageStats['lightWinter']}} час.</div>
+                <div class="stat-text">интенсивность освещения: {{$averageStats['light']}} </div>
+                <div class="stat-text">влажность: {{$averageStats['wet']}} </div>
             @endif
         </div>
         <div class="searchResult-wrapper">
             @if($searchResult)
                 @foreach($searchResult as $plant)
                     <div wire:key="{{$plant->id}}" class="plant-card">
-                        <img class="cart-image" src="{{Storage::disk('public')->url($plant->image)}}" alt="plant">
-                        <h4>{{$plant->name}}</h4>
+                        <img x-on:click="$dispatch('open-modal',{modalID:'{{$plant->id}}'})" class="cart-image" src="{{Storage::disk('public')->url($plant->image)}}" alt="plant">
+                        <h4 x-on:click="$dispatch('open-modal',{modalID:'{{$plant->id}}'})">{{$plant->name}}</h4>
+                    </div>
+                    <div wire:key="{{$plant->id}}-modal" class="modal"
+                         x-data="{open:false, modalID:'{{$plant->id}}'}"
+                         x-show="open"
+                         x-on:open-modal.window="open=($event.detail.modalID === modalID)"
+                         x-on:close-modal.window="open=false"
+                         x-transition.duration.700ms
+                         style="display: none;">
+                        <div class="modal-header">
+                            <h4 class="modal-title">{{$plant->name}}</h4>
+                            <button x-on:click="open=false" class="modal-close">X</button>
+                        </div>
+                        <div class="modal-body">
+                            <img class="modal-image" src="{{Storage::disk('public')->url($plant->image)}}" alt="plant">
+                        </div>
                     </div>
                 @endforeach
             @endif
