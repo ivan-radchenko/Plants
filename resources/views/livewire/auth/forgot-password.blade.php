c76d5feb4efa4e78a4f77bb4e9cf640892ef920d
Ivan <sonysakura.rf@gmail.com>
2024-02-01 19:51:41 +0300

    register/forgot/reset DONE

--- resources/views/livewire/auth/forgot-password.blade.php
+++ resources/views/livewire/auth/forgot-password.blade.php
@@ -1,19 +1,22 @@
 <div>
     <link href="{{ asset('css/auth/forgotPassword.css') }}" rel="stylesheet">
     <div class="wrapper">
-        <form  wire:submit="send" class="form" method="post">
-            @csrf
+        <div class="messages">
             @if (session('status'))
-                <span class="status">{{ session('status') }}</span>
+                <span class="error">{{ session('status') }}</span>
             @endif
             @foreach ($errors->all() as $error)
-                <li class="error">{{ $error }}</li>
+                <span class="error">{{ $error }}</span>
             @endforeach
+        </div>
+        <form  wire:submit="send" class="form" method="post">
+            @csrf
             <div class="email form-item">
                 <label for="email">Email</label>
                 <input wire:model="email" class="form-input" type="email" name="email" id="email">
             </div>
             <button class="button" type="submit">Отправить</button>
         </form>
+        <img src="{{asset('images/auth/forgot-password-plants.svg')}}" alt="" class="forgot-password-plants">
     </div>
 </div>
