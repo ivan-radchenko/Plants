25b5c2e89a9e3c977d8bf2b9655f1ab47eb72613
Ivan <sonysakura.rf@gmail.com>
2024-02-29 12:33:46 +0300

    watering done

--- resources/views/livewire/care-today.blade.php
+++ resources/views/livewire/care-today.blade.php
@@ -1,27 +1,38 @@
-<div>
+                <div>
     <link href="{{ asset('css/care-today.css') }}" rel="stylesheet">
     <div class="wrapper">
-        <div>
+        <div class="sub-header">
+            <img src="{{asset('images/header/left-subheader-image.svg')}}" alt="" class="left-subheader-image">
+            <img src="{{asset('images/header/right-subheader-image.svg')}}" alt="" class="right-subheader-image">
+            <div class="messages">
+                @if (session('status'))
+                    <span class="error">{{ session('status') }}</span>
+                @endif
+                @foreach ($errors->all() as $error)
+                    <span class="error">{{ $error }}</span>
+                @endforeach
+            </div>
+        </div>
+        <div class="plants-wrapper">
         @if($plants)
-            <h3>Полив:</h3>
+            <div class="water-wrapper">
+                <h3 class="section-title">Полив:</h3>
+                <button wire:click="wateringAll" type="button" id="water-plant-all" class="button">
+                    Полить все
+                </button>
+            </div>
             <div class="cards-wrapper" wire:ignore>
                 @foreach($plants as $plant)
-                    <div wire:key="{{$plant->id}}"  class="plant-card">
+                    <div wire:key="{{$plant->id}}"  class="plant-card" i>
                         <img class="cart-image" src="{{Storage::disk('public')->url($plant->image)}}" alt="plant">
                         <div>{{$plant->name}}</div>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="drop" id="drop{{$plant->id}}" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M7.21.8C7.69.295 8 0 8 0c.109.363.234.708.371 1.038.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8zm.413 1.021A31.25 31.25 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10a5 5 0 0 0 10 0c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"/>
-                            <path fill-rule="evenodd" d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87z"/>
+                        <svg wire:click="wateringThis({{$plant->id}})" id="drop{{$plant->id}}" data-name="drop{{$plant->id}}" class="drop"  xmlns="http://www.w3.org/2000/svg" width="69.46" height="93.9" viewBox="0 0 69.46 93.9">
+                            <path d="M35.13,1.5s20.01,31.27,27.68,43.14c7.67,11.86,6.9,27.27-3.12,37.44-10.03,10.17-24.55,10.32-24.55,10.32,0,0-13.52.31-24.43-10.01C-.21,72.06-.53,56.29,5.12,47.72,9.19,41.53,35.13,1.5,35.13,1.5Z" style="fill: none; stroke: #9df2db; stroke-linecap: round; stroke-linejoin: round; stroke-width: 3px;"/>
+                            <path d="M14.81,63.14s1.81,16.77,16.77,15.94" style="fill: none; stroke: #9df2db; stroke-linecap: round; stroke-miterlimit: 10; stroke-width: 3px;"/>
                         </svg>
-                        <button wire:click="wateringThis({{$plant->id}})" id="water-plant{{$plant->id}}" type="button">
-                            полить
-                        </button>
                     </div>
                 @endforeach
             </div>
-            <button wire:click="wateringAll" type="button" id="water-plant-all">
-                полить все
-            </button>
         @else
             <div>just расслабься</div>
         @endif
@@ -80,7 +91,7 @@
         @if($plants)
         @foreach($plants as $plant)
 
-        document.getElementById("water-plant{{$plant->id}}").addEventListener("click", function() {
+        document.getElementById("drop{{$plant->id}}").addEventListener("click", function() {
             document.getElementById("drop{{$plant->id}}").classList.add("open")
         });
 
