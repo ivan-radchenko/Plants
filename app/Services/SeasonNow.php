1c8cfdbe01cb183e129ae9f906f485dd1f941919
Ivan <sonysakura.rf@gmail.com>
2024-12-01 12:44:06 +0300

    fix: winter period

--- app/Services/SeasonNow.php
+++ app/Services/SeasonNow.php
@@ -10,7 +10,7 @@ class SeasonNow
     public static function season(): string
     {
         $monthNow=Carbon::now()->month;
-        if ($monthNow === 11 || $monthNow <= 2 ){
+        if ($monthNow >= 11 || $monthNow <= 2 ){
             return PlantStatus::WINTER->value;
         } else{
             return PlantStatus::SUMMER->value;
